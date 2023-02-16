11338d97dc43dda26b337aac9e2d3ec61d8800a2
Teresa <tenjerim@gmail.com>
2023-02-16 06:24:48 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student 
 
+     def initialize
+        @knowledge = []
+    end
+   
+   def learn(knowledge)
+     @knowledge = knowledge
+   end
+
+   def knowledge
+    @knowledge
+   end
+
+   def self.knowledge
+    @@knowledge
+   end
+
 end
